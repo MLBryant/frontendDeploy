2b2bab94cc048ca1dbfa0a3459e004bdfa45a2ef
Mason Bryant <mlbryant6@gmail.com>
2021-11-17 16:40:00 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use('/css', express.static(path.join(__dirname, 'style.css')))
+app.use('/js', express.static(path.join(__dirname, 'profile.js')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'))
+})
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, './style.css'))
+})
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, './profile.js'))
+})
+
+const server = process.env.PORT || 4000
+app.listen(server, () => console.log(`Server is running on ${server}`))
